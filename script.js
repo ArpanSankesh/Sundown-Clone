1c173e8cf2a310c07f86b8e27acada84af0d0d7f
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-27 20:16:36 +0530

    progress update

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var projects = document.querySelector('.featured-projects');
+var fixedImages = document.querySelector('#fixed-images');
+
+
+projects.addEventListener('mouseenter', function(){
+    fixedImages.style.display = "block";
+})
+projects.addEventListener('mouseleave', function(){
+    fixedImages.style.display = "none";
+})
