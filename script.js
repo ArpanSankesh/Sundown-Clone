ccf2af673669f1d4475941b75b48dfc04f0d24c1
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-27 21:51:00 +0530

    updates

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
 var projects = document.querySelector('.featured-projects');
 var fixedImages = document.querySelector('#fixed-images');
+var project = document.querySelectorAll('.project') 
 
 
 projects.addEventListener('mouseenter', function(){
     fixedImages.style.display = "block";
-})
+});
 projects.addEventListener('mouseleave', function(){
     fixedImages.style.display = "none";
-})
+});
+
+
+project.forEach(function(e){
+    e.addEventListener('mouseenter', function(){
+        var image = e.getAttribute('data-image');
+        console.log(image)
+        fixedImages.style.backgroundImage = `url(${image})`;
+    });
+});
