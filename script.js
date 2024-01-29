214b02d58feb99df50d094408535cc7e4be15ef6
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-30 00:50:17 +0530

    updates

--- script.js
+++ script.js
@@ -3,35 +3,38 @@
 //     smooth: true
 // });
 
-var projects = document.querySelector('.featured-projects');
-var fixedImages = document.querySelector('#fixed-images');
-var project = document.querySelectorAll('.project') 
+function pageFourAnimation() {
+  var projects = document.querySelector(".featured-projects");
+  var fixedImages = document.querySelector("#fixed-images");
+  var project = document.querySelectorAll(".project");
 
-
-projects.addEventListener('mouseenter', function(){
+  projects.addEventListener("mouseenter", function () {
     fixedImages.style.display = "block";
-});
-projects.addEventListener('mouseleave', function(){
+  });
+  projects.addEventListener("mouseleave", function () {
     fixedImages.style.display = "none";
-});
+  });
 
-
-project.forEach(function(e){
-    e.addEventListener('mouseenter', function(){
-        var image = e.getAttribute('data-image');
-        console.log(image)
-        fixedImages.style.backgroundImage = `url(${image})`;
+  project.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      console.log(image);
+      fixedImages.style.backgroundImage = `url(${image})`;
     });
-});
+  });
+}
 
-var swiper = new Swiper(".mySwiper", {
+function swiperJsAnimation() {
+  var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     centeredSlides: true,
-    spaceBetween: 30,
+    spaceBetween: 100,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
   });
+}
 
-
+swiperJsAnimation()
+pageFourAnimation()
