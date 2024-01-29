955d2970b671d398e127e8c067482a15e28b9f78
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-30 00:14:09 +0530

    swiper JS initilized

--- script.js
+++ script.js
@@ -24,4 +24,14 @@ project.forEach(function(e){
     });
 });
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 
