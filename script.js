1e0d302dc8b2e1d7e7d6a23c2b80ed2f25394fef
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-31 19:08:22 +0530

    updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
 function pageFourAnimation() {
   var projects = document.querySelector(".featured-projects");
